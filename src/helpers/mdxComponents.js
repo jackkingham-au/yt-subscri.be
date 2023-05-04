2dda62ef05d93d8433f4c62847e248ef6453e14c
Jack Kingham <kinghamjack12@gmail.com>
2023-05-04 21:15:46 +0700

    Add First Blog Post

--- src/helpers/mdxComponents.js
+++ src/helpers/mdxComponents.js
@@ -8,5 +8,9 @@ export default {
     ),
     img: (props) => (
         <Image { ...props } width={300} height={300} className="rounded-md" />
+    ),
+    a: (props) => (
+        <a { ...props } target="_blank" rel="noreferrer nooepner" className='underline text-red-500'>{props.children}</a>
     )
+
 }
